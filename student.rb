22e07c1024bddc8673b648ee29b64ea9e604c6f6
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-03-02 15:36:42 +0100

    Create Classroom class

--- student.rb
+++ student.rb
@@ -1,9 +1,12 @@
 require './person'
 
 class Student < Person
+  attr_writer :classroom
+
   def initialize(age, name, parent_permission, classroom)
     super(age, name, parent_permission)
-    @classroom = classroom
+    @classroom = classroom # not needed but better safe than sorry
+    classroom.add_student(self)
   end
 
   def play_hooky
